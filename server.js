d6afce2e381ac676ff8a9b3fd161598a3dbd4f8d
let565047-boop <let565047@gmail.com>
2026-05-12 10:36:49 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,105 +1,170 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Phục vụ file tĩnh (html, css, js)
 app.use(express.static(__dirname));
 
-// Biến lưu trữ các phòng game
 let gameRooms = {};
 
-io.on("connection", (socket) => {
-    console.log("Thiết bị kết nối mới:", socket.id);
+// 5 Loại bản đồ cực cháy
+const maps = {
+    classic: "RANDOM",
+    arena: [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,1,1,1,0,0,0,0,1],
+        [1,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+        [1,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+        [1,0,0,0,0,1,1,1,1,1,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ],
+    cross: [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,0,0,0,0,0,0,0,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ],
+    zigzag: [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ],
+    fortress: [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,1,1,0,1,1,0,1,1,0,1,1,0,1],
+        [1,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+        [1,0,0,1,1,1,0,0,0,1,1,1,0,0,1],
+        [1,1,0,0,0,0,0,1,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,1,0,0,0,0,0,1,1],
+        [1,0,0,1,1,1,0,0,0,1,1,1,0,0,1],
+        [1,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+        [1,0,1,1,0,1,1,0,1,1,0,1,1,0,1],
+        [1,0,1,1,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ]
+};
 
-    // 1. Xử lý khi sen nhấn "Vào Phòng"
+io.on("connection", (socket) => {
     socket.on("join-room", (data) => {
         const { roomId, playerName, color } = data;
-        
-        // Cho socket gia nhập vào "kênh" riêng của phòng đó
         socket.join(roomId);
+        if (!gameRooms[roomId]) gameRooms[roomId] = { players: {}, items: [] };
 
-        // Nếu phòng chưa tồn tại thì tạo mới
-        if (!gameRooms[roomId]) {
-            gameRooms[roomId] = { players: {} };
-        }
-
-        // Người đầu tiên vào phòng sẽ là chủ phòng (Host)
         const isHost = Object.keys(gameRooms[roomId].players).length === 0;
-
-        // Lưu thông tin người chơi vào phòng
         gameRooms[roomId].players[socket.id] = {
-            id: socket.id,
-            name: playerName,
-            color: color,
-            x: 60, // Tọa độ xuất phát
-            y: 60,
-            angle: 0,
-            isHost: isHost,
-            alive: true
+            id: socket.id, name: playerName, color: color,
+            x: 60, y: 60, angle: 0, isHost: isHost, alive: true, 
+            hp: 100, speed: 3, damage: 35, fireRate: 600
         };
-
-        // Gửi danh sách người chơi mới nhất cho TẤT CẢ mọi người TRONG PHÒNG đó
         io.to(roomId).emit("update-players", gameRooms[roomId].players);
-        console.log(`Sen ${playerName} đã vào phòng: ${roomId}`);
     });
 
-    // 2. Xử lý khi chủ phòng nhấn "Khai chiến"
-    socket.on("start-game", (roomId) => {
-        io.to(roomId).emit("game-started");
+    socket.on("start-game", (data) => {
+        const { roomId, mapType } = data;
+        if (gameRooms[roomId]) {
+            let finalMap = maps[mapType] || "RANDOM";
+            gameRooms[roomId].items = [];
+            const types = ['SPEED', 'DAMAGE', 'HEAL'];
+            for(let i=0; i<5; i++) {
+                gameRooms[roomId].items.push({
+                    id: Math.random(),
+                    x: Math.floor(Math.random() * 13 + 1) * 40 + 20,
+                    y: Math.floor(Math.random() * 13 + 1) * 40 + 20,
+                    type: types[Math.floor(Math.random() * types.length)]
+                });
+            }
+            io.to(roomId).emit("game-started", { map: finalMap });
+            io.to(roomId).emit("spawn-items", gameRooms[roomId].items);
+        }
     });
 
-    // 3. Xử lý di chuyển
+    socket.on("item-collected", (data) => {
+        const { roomId, itemId, type } = data;
+        if (gameRooms[roomId]) {
+            // ĐỒNG BỘ MÁU: Nếu ăn túi hồi máu, server cập nhật máu ngay
+            if (type === 'HEAL' && gameRooms[roomId].players[socket.id]) {
+                let p = gameRooms[roomId].players[socket.id];
+                p.hp = Math.min(100, p.hp + 40);
+            }
+            
+            gameRooms[roomId].items = gameRooms[roomId].items.filter(i => i.id !== itemId);
+            io.to(roomId).emit("spawn-items", gameRooms[roomId].items);
+            io.to(roomId).emit("update-players", gameRooms[roomId].players);
+        }
+    });
+
     socket.on("move", (data) => {
         const { roomId, x, y, angle } = data;
-        if (gameRooms[roomId] && gameRooms[roomId].players[socket.id]) {
-            gameRooms[roomId].players[socket.id].x = x;
-            gameRooms[roomId].players[socket.id].y = y;
-            gameRooms[roomId].players[socket.id].angle = angle;
-            // Chỉ gửi tọa độ cho những người KHÁC trong cùng phòng
+        if (gameRooms[roomId]?.players[socket.id]) {
+            Object.assign(gameRooms[roomId].players[socket.id], { x, y, angle });
             socket.to(roomId).emit("player-moved", { id: socket.id, x, y, angle });
         }
     });
 
-    // 4. Xử lý bắn đạn
-    socket.on("shoot", (bullet) => {
-        // Gửi viên đạn tới những người khác trong phòng
-        socket.to(bullet.roomId).emit("new-bullet", bullet);
-    });
+    socket.on("shoot", (bullet) => socket.to(bullet.roomId).emit("new-bullet", bullet));
 
-    // 5. Xử lý khi xe tăng bị bắn trúng
     socket.on("hit", (data) => {
-        const { roomId, targetId } = data;
-        if (gameRooms[roomId] && gameRooms[roomId].players[targetId]) {
-            gameRooms[roomId].players[targetId].alive = false;
-            io.to(roomId).emit("player-hit", targetId);
+        const { roomId, targetId, damage } = data;
+        const room = gameRooms[roomId];
+        if (room && room.players[targetId] && room.players[targetId].alive) {
+            room.players[targetId].hp -= damage;
+            
+            if (room.players[targetId].hp <= 0) {
+                room.players[targetId].hp = 0; 
+                room.players[targetId].alive = false;
+                io.to(targetId).emit("game-over", { result: "DEFEAT" });
+                socket.emit("game-over", { result: "VICTORY" });
+                setTimeout(() => io.to(roomId).emit("return-to-lobby"), 4000);
+            }
+            // Gửi cập nhật để thanh máu địch tụt ngay lập tức
+            io.to(roomId).emit("update-players", room.players);
         }
     });
 
-    // 6. Xử lý khi sen thoát game
     socket.on("disconnect", () => {
         for (let roomId in gameRooms) {
             if (gameRooms[roomId].players[socket.id]) {
-                console.log(`Người chơi ${gameRooms[roomId].players[socket.id].name} đã thoát`);
                 delete gameRooms[roomId].players[socket.id];
-                
-                // Cập nhật lại danh sách cho người còn lại
                 io.to(roomId).emit("update-players", gameRooms[roomId].players);
-                
-                // Nếu phòng trống rỗng thì xóa luôn phòng cho nhẹ server
-                if (Object.keys(gameRooms[roomId].players).length === 0) {
-                    delete gameRooms[roomId];
-                }
+                if (Object.keys(gameRooms[roomId].players).length === 0) delete gameRooms[roomId];
             }
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server đang chạy trên cổng ${PORT}`);
-    console.log(`Chiến thôi sen ơi: http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server chạy tại cổng ${PORT}`));
